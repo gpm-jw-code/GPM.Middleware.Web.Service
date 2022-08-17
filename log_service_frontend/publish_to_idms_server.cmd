5a1d771ec842f49c4831d20defeeb8626323c9d3
jinwei69 <ss610349@gmail.com>
2022-08-17 19:25:16 +0800

    mmm

--- log_service_frontend/publish_to_idms_server.cmd
+++ log_service_frontend/publish_to_idms_server.cmd
@@ -1,2 +1,2 @@
-robocopy "D:\專案\gmm\Code\gmm.projects\web.service\wwwroot" "\\DESKTOP-74GGIEJ\Users\User\Desktop\IDMS\Server\wwwroot" /E
+robocopy "D:\專案\gmm\Code\gmm.projects\web.service\wwwroot" "\\192.168.0.123\Users\User\Desktop\IDMS\Server\wwwroot" /E
 robocopy "D:\專案\gmm\Code\gmm.projects\web.service\wwwroot" "D:\專案\IDMS\Smart IOT\IDMSWebServer\wwwroot" /E
